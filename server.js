15d5b3a17f16f9b65d67d59fb6a935674da31d8f
dev-weirdo <dev_weirdo@protonmail.com>
2022-05-23 23:59:42 +0600

    Added tools path

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
+//Packages
+
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+//DB Connect
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tndro.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const toolsCollection = client.db("electric-tools-manufacturer").collection("tools");
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+        })
+    }
+    finally { }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
