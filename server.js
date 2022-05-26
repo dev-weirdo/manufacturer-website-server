dd8a8b8f8ccbf581ff3226861a01cc706d7f7fa9
dev-weirdo <dev_weirdo@protonmail.com>
2022-05-26 22:56:08 +0600

    added reviews api

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const run = async () => {
         await client.connect();
         const toolsCollection = client.db("electric-tools-manufacturer").collection("tools");
         const ordersCollection = client.db("electric-tools-manufacturer").collection("orders");
+        const reviewsCollection = client.db("electric-tools-manufacturer").collection("reviews");
 
         app.get('/tools', async (req, res) => {
             const query = {};
@@ -30,17 +31,20 @@ const run = async () => {
             const tools = await cursor.toArray();
             res.send(tools);
         })
+
         app.get('/tools/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const tool = await toolsCollection.findOne(query);
             res.send(tool);
         })
+
         app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await ordersCollection.insertOne(order);
             res.send(result);
         })
+
         app.get('/orders', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
@@ -48,11 +52,26 @@ const run = async () => {
             const tools = await cursor.toArray();
             res.send(tools);
         })
+
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await ordersCollection.deleteOne(query);
+            console.log(id);
+            // const query = { _id: ObjectId(id) };
+            // const result = await ordersCollection.deleteOne(query);
+            // res.send(result);
+        })
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
             res.send(result);
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewsCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         })
     }
     finally { }
