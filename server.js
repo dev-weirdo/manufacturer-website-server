f0eb51be4a1fcdd026987177dc9f7846dbaf7742
dev-weirdo <dev_weirdo@protonmail.com>
2022-05-28 14:58:41 +0600

    Manage Products page handled

--- server.js
+++ server.js
@@ -120,7 +120,6 @@ const run = async () => {
 
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
             const query = { _id: ObjectId(id) };
             const result = await ordersCollection.deleteOne(query);
             res.send(result);
